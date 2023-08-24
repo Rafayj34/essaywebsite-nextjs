9650e99b02f18a029590c68bd5edb8d133394909
Arham Baig <arhambaig.amir@gmail.com>
2023-08-24 11:52:02 +0500

    dploying

--- app/api/upload/route.js
+++ app/api/upload/route.js
@@ -16,8 +16,9 @@ export async function POST(request) {
     const bytes = await file.arrayBuffer();
     const buffer = Buffer.from(bytes);
 
-    const filePath = path.join('public', 'temp', file.name);
+    const filePath = path.join(process.cwd(), 'public', 'temp', file.name);
     await writeFile(filePath, buffer);
+    
     console.log(`File ${file.name} uploaded to ${filePath}`);
 
     attachments.push({
